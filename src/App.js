13b83bac4b389679d4190129b70ed0b08a324691
vitorcrl <vitor.carlos_souza@hotmail.com>
2020-11-03 21:26:33 -0300

    teste 1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import api from "./services/api";
+export default App;
 import "./styles.css";
 
+
 function App() {
+
+const [repositories, setRepositories] = useState([]);
+
+  useEffect(() =>{
+    api.get('repository').then(response=> {
+      setRepository(response.data)
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title:'Vitor',
+      url:'https://github.com/vitorcrl',
+      techs:['Node.js', 'React.js']
+    })
+    setRepositories([...repositories, response.data])
   }
+ api.post('repository',{
+      title:`Novo  ${Date.now()}`,
+  });
 
   async function handleRemoveRepository(id) {
-    // TODO
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
+       {repositories.map(repository => (
+          <li key={repository.id}>
+             {repository.title}
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
-        </li>
+         </li>
+         ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
+  
     </div>
-  );
-}
-
-export default App;
+    );
+       }     
